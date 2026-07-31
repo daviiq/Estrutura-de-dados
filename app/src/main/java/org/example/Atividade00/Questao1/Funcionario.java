1e10cfd1be383489989f8f71b3d6fe2381210e8c
RamosRrG <gustavo.r16@aluno.ifsc.edu.br>
2026-07-31 14:51:13 -0300

    criado a estrutura dos funcionarios e do gerente e calculo do salario. Dentro do projeto precisa adicionar as variaveis que estão sendo utilizadas para o calculo do Salario. Para chamar precisa do projeto para que não qubre o polimorfismo da classe gerente que precisa estar calculando o salario em cima da quantidade de funcionarios do projeto. Ira precisar de um metodo int getFuncionarios e string getStatus para saber se o projeto esta finalizado ou não

--- app/src/main/java/org/example/Atividade00/Questao1/Funcionario.java
+++ app/src/main/java/org/example/Atividade00/Questao1/Funcionario.java
@@ -1,4 +1,19 @@
 package org.example.Atividade00.Questao1;
 
 public class Funcionario {
+    protected String nome;
+    protected double salarioBase;
+
+    public double getSalario(Projeto projeto){
+        if(projeto.getStatus().equals("finalizado"){
+            return salarioBase * 1.1;
+        }
+        return salarioBase;
+    }
+
+    public Funcionario(String nome, double salarioBase){
+        this.salarioBase = salarioBase;
+        this.nome = nome;
+    }
+
 }
--- app/src/main/java/org/example/Atividade00/Questao1/Gerente.java
+++ app/src/main/java/org/example/Atividade00/Questao1/Gerente.java
@@ -1,4 +1,17 @@
 package org.example.Atividade00.Questao1;
 
-public class Gerente {
+public class Gerente extends Funcionario{
+    public Gerente(String nome, double salarioBase) {
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public double getSalario(Projeto projeto){
+        salarioBase = salarioBase*(1+getFuncionarios()/100);
+
+        if(projeto.getStatus().equals("finalizado"){
+            return salarioBase * 1.1;
+        }
+        return salarioBase;
+    }
 }
